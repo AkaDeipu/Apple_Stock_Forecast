4f50bd268248554d5e56369fc31ad6b8a000c6c4
Santosh Reddy <santoshr3dd4@gmail.com>
2025-12-20 11:51:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ class ForecastRequest(BaseModel):
 
 @app.get("/", response_class=HTMLResponse) 
 def home(request: Request): 
-    return templates.TemplateResponse("index.html", {"request": request})
+    return templates.TemplateResponse("ui.html", {"request": request})
 
 @app.post("/predict")
 def predict(request: ForecastRequest):
